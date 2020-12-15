517aed136d630ebff7f7ed2673389a443fb0b1be
Ubuntu <ubuntu@ip-172-31-9-109.eu-west-1.compute.internal>
2020-12-15 10:43:05 +0000

    pruebas

--- miproyecto/src/main/java/es/curso/App.java
+++ miproyecto/src/main/java/es/curso/App.java
@@ -16,10 +16,11 @@ public class App
         return numero*2;
     }
     
+    
     public static int mayor( int numero1, int numero2 )
     {
         if (numero1 > numero2){
-        return numero1
+        return numero1;
         }
         return numero2;
 
